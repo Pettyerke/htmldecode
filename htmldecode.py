7828c37fda483a1e654239f6af83a68a8b6ee002
Pettyerke <petty4545@hotmail.com>
2013-09-18 20:42:50 +0200

    First commit
    Decoding and encoding text.
    Keymapping: CTRL+H CTRL+E and CTRL+H CTRL+D

---
+++ htmldecode.py
@@ -0,0 +1,36 @@
+import sublime, sublime_plugin
+
+# Created by pberezvay
+# 2013.09.18
+
+class htmldecodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		regions = view.sel()
+		if len(regions) > 1 or not regions[0].empty():
+			for region in view.sel():
+				if not region.empty():
+					s = view.substr(region)
+					s =	s.replace('&lt;','<').replace('&gt;','>')
+					view.replace(edit, region,s)
+		else:
+			alltextreg = sublime.Region(0, view.size())
+			s = view.substr(alltextreg)
+			s =	s.replace('&lt;','<').replace('&gt;','>')
+			view.replace(edit, alltextreg, s)
+
+class htmlencodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		regions = view.sel()
+		if len(regions) > 1 or not regions[0].empty():
+			for region in view.sel():
+				if not region.empty():
+					s = view.substr(region)
+					s =	s.replace('<','&lt;').replace('>','&gt;')
+					view.replace(edit, region,s)
+		else:
+			alltextreg = sublime.Region(0, view.size())
+			s = view.substr(alltextreg)
+			s =	s.replace('<','&lt;').replace('>','&gt;')
+			view.replace(edit, alltextreg, s)
